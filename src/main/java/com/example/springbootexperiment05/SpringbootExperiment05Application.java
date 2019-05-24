9bee4c11f0ac37e0c8c8752ef00303e80e6b3a52
75113 <e1172090224@163.com>
2019-05-24 17:31:33 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootexperiment05/SpringbootExperiment05Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootexperiment05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExperiment05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootExperiment05Application.class, args);
+    }
+
+}
